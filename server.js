ebf64091004bf6ae51744c144454be56b4fa1114
ShrutiKhambhay <62984207+ShrutiKhambhay@users.noreply.github.com>
2021-01-30 16:49:12 +0530

    message

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT||8000;
+const port = process.env.PORT||80;
 const randData = (min,max) => {
     return Math.floor(Math.random() * (max-min)+ min);
 };
